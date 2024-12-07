39b204abd0e2e2b28f8189ebc6f78cda9157b305
Lucas Hauptmann Pereira <lucass.hauptmann@gmail.com>
2024-12-07 11:31:12 -0300

    Update user flow on landing page

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'pages/index'
-  root 'pages#landing_page'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  authenticated(:user) do
+    root "pages#index", as: :authenticated_root
+  end
+
+  unauthenticated(:user) do
+    root "pages#landing_page"
+  end
 end
