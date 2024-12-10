97a8fb6351f8bff07453254acdeb185bf062b013
Lucas Hauptmann Pereira <lucass.hauptmann@gmail.com>
2024-12-09 22:00:08 -0300

    Create Section model and evaluation and chapter resources

---
+++ app/controllers/evaluations_controller.rb
@@ -0,0 +1,56 @@
+class EvaluationsController < ApplicationController
+  before_action :set_evaluation, only: %i[ show edit update destroy ]
+
+  def index
+    @evaluations = Section.all
+  end
+
+  def show
+  end
+
+  def new
+    @evaluation = Section.new
+  end
+
+  def edit
+  end
+
+  def create
+    @evaluation = current_user.sections.build(evaluation_params)
+
+    respond_to do |format|
+      if @evaluation.save
+        format.html { redirect_to @evaluation, notice: "Evaluation was successfully created." }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def update
+    respond_to do |format|
+      if @evaluation.update(evaluation_params)
+        format.html { redirect_to @evaluation, notice: "Evaluation was successfully updated." }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @evaluation.destroy
+
+    respond_to do |format|
+      format.html { redirect_to evaluations_path, status: :see_other, notice: "Evaluation was successfully destroyed." }
+    end
+  end
+
+  private
+    def set_evaluation
+      @evaluation = Evaluation.find(params[:id])
+    end
+
+    def evaluation_params
+      params.require(:section).permit(:name, :description, :type, :body, :user_id)
+    end
+end
